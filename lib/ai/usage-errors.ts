21fbec59fdd89f75f88907995b6ae9223492b7a4
Abhishek Dubey <83163959+abhishekduey6676@users.noreply.github.com>
2026-09-20 21:14:57 +0530

    Delete lib directory at root

--- lib/ai/usage-errors.ts
+++
@@ -1,6 +0,0 @@
-export class AnalysisLimitError extends Error {
-  constructor(message: string, readonly retryAfterSeconds: number) {
-    super(message);
-    this.name = 'AnalysisLimitError';
-  }
-}
